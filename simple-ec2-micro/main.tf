3d86f019b8bc0d3d3a3ebeb40d02347dba3c971c
dimallya <dimallya@in.ibm.com>
2022-07-28 10:00:59 +0530

    templates commit

---
+++ simple-ec2-micro/main.tf
@@ -0,0 +1,39 @@
+data "aws_vpc" "selected" {
+  state = "available"
+
+  filter {
+    name   = "tag:Name"
+    values = ["${var.vpc_name_tag}"]
+  }
+}
+
+data "aws_subnet" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.subnet_name}"]
+  }
+}
+
+# Lookup for AMI based on image name and owner ID
+data "aws_ami" "aws_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["${var.aws_image}*"]
+  }
+
+  owners = ["${var.aws_ami_owner_id}"]
+}
+
+resource "aws_key_pair" "orpheus_public_key" {
+  key_name   = "${var.public_ssh_key_name}"
+  public_key = "${var.public_ssh_key}"
+}
+
+resource "aws_instance" "orpheus_ubuntu_micro" {
+  instance_type = "${var.aws_image_size}"
+  ami           = "${data.aws_ami.aws_ami.id}"
+  subnet_id     = "${data.aws_subnet.selected.id}"
+  key_name      = "${aws_key_pair.orpheus_public_key.id}"
+}
---
+++ simple-ec2-micro/variable.tf
@@ -0,0 +1,46 @@
+variable "access_key"{
+  description = "AWS Access Key."
+}
+
+variable "secret_key"{
+  description = "AWS Secret Key."
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
+
+variable "vpc_name_tag" {
+  description = "Name of the Virtual Private Cloud (VPC) this resource is going to be deployed into"
+}
+
+variable "subnet_name" {
+  description = "Subnet Name"
+}
+
+variable "aws_image_size" {
+  description = "AWS Image Instance Size"
+  default     = "t2.small"
+}
+
+
+variable "public_ssh_key_name" {
+  description = "Name of the public SSH key used to connect to the virtual guest"
+}
+
+variable "public_ssh_key" {
+  description = "Public SSH key used to connect to the virtual guest"
+}
+
+#Variable : AWS image name
+variable "aws_image" {
+  type        = string
+  description = "Operating system image id / template that should be used when creating the virtual image"
+  default     = "ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"
+}
+
+variable "aws_ami_owner_id" {
+  description = "AWS AMI Owner ID"
+  default     = "099720109477"
+}
